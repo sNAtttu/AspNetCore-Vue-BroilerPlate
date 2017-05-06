d8b9004845bee862ac78e1326790682ed7c670a3
Santeri Nousiainen <santeri.nousiainen@ecraft.com>
2017-05-06 22:02:56 +0300

    Now images are handled correctly if they are used in VUE single file compontents.

--- BroileriLautanen/webpack.config.js
+++ BroileriLautanen/webpack.config.js
@@ -26,6 +26,13 @@ module.exports = {
       {
         test: /\.vue$/,
         loader: 'vue'
+      },
+      {
+          test: /\.(jpe?g|png|gif|svg)$/i,
+          loaders: [
+              'file-loader?hash=sha512&digest=hex&name=[hash].[ext]',
+              'image-webpack-loader?bypassOnDebug&optimizationLevel=7&interlaced=false'
+          ]
       }
     ]
   },
